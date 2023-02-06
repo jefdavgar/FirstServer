83a0d4ef00a02e74e434f92a04980b87ce8a6905
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-02-06 13:44:36 -0500

    first server whit express js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hola jeff");
+const express = require("express");
+const app = express()
+const port = 3000
+app.get( "/", (req, res)=>{
+    res.send("Hello jeff")
+})
+app.listen(port, () => {
+    console.log("My port is: "+ port)
+})
